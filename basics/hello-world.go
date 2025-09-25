2e7cfce7d921a7961e385e721c4054bce4144238
stephanie <stephanieha2021@gmail.com>
2025-09-24 23:08:33 -0400

    first hello world

---
+++ basics/hello-world.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	message := "Hello World!"
+	fmt.Println("Hello World!")
+	fmt.Printf("%s\n", message)
+
+	// to run a go file, "$ go run file-name.go"
+}
